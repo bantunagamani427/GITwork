9992799374465bba6abe5c0082d5092c8ce7a8ca
LENOVO <LENOVO@DESKTOP-E57397J>
2022-07-29 17:03:25 +0530

    Committing to contact

---
+++ src/test/java/CONTACT/Contact1.java
@@ -0,0 +1,9 @@
+package CONTACT;
+
+public class Contact1 {
+
+	public static void main(String[] args) {
+System.out.println("from contact1");
+	}
+
+}
---
+++ src/test/java/CONTACT/Contact2.java
@@ -0,0 +1,9 @@
+package CONTACT;
+
+public class Contact2 {
+
+	public static void main(String[] args) {
+System.out.println("from contact2");
+	}
+
+}
