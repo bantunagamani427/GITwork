b69e308749a529948a5cb39e965794b6018a792b
LENOVO <LENOVO@DESKTOP-E57397J>
2022-07-29 18:03:24 +0530

    Nagamani package

---
+++ src/test/java/Nagamani/Mani.java
@@ -0,0 +1,13 @@
+package Nagamani;
+
+public class Mani {
+
+	public static void main(String[] args) {
+System.out.println("bannu");
+System.out.println("bittu");
+System.out.println("bava");
+System.out.println("naga");
+
+	}
+
+}
