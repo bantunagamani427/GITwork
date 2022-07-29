8f288b7380c0c4879dfc6cb38897ccfebcf040ac
LENOVO <LENOVO@DESKTOP-E57397J>
2022-07-29 17:07:52 +0530

    committing to campaign

---
+++ src/test/java/CAMPAIGN/Campaign1.java
@@ -0,0 +1,9 @@
+package CAMPAIGN;
+
+public class Campaign1 {
+
+	public static void main(String[] args) {
+System.out.println("from campaign");
+	}
+
+}
---
+++ src/test/java/CAMPAIGN/Campaign2.java
@@ -0,0 +1,9 @@
+package CAMPAIGN;
+
+public class Campaign2 {
+
+	public static void main(String[] args) {
+System.out.println("from campaign2");
+	}
+
+}
