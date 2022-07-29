ecc6cab7010fc1120234d020f3ca1a288f6b40f6
LENOVO <LENOVO@DESKTOP-E57397J>
2022-07-29 16:34:16 +0530

    committing to master

---
+++ src/test/java/Master/Master1.java
@@ -0,0 +1,9 @@
+package Master;
+
+public class Master1 {
+
+	public static void main(String[] args) {
+System.out.println("from master 1 ");
+	}
+
+}
---
+++ src/test/java/Master/Master2.java
@@ -0,0 +1,9 @@
+package Master;
+
+public class Master2 {
+
+	public static void main(String[] args) {
+System.out.println("from master 2");
+	}
+
+}
